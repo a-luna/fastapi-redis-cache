2895439e639961b74a23e90da170e0ac484168c0
Aaron Luna <contact@aaronluna.dev>
2021-04-23 02:22:19 -0700

    refactor cache and client modules (#4)

--- src/fastapi_redis_cache/cache.py
+++ src/fastapi_redis_cache/cache.py
@@ -6,6 +6,8 @@ from functools import wraps
 from http import HTTPStatus
 from typing import Union
 
+from fastapi import Response
+
 from fastapi_redis_cache.client import FastApiRedisCache
 
 
@@ -33,44 +35,57 @@ def cache(
     def outer_wrapper(func):
         @wraps(func)
         async def inner_wrapper(*args, **kwargs):
-            """Return cached value if one exists, otherwise the value returned by the function is added to the cache."""
+            """Return cached value if one exists, otherwise evaluate the wrapped function and cache the result."""
+
             func_kwargs = kwargs.copy()
             request = func_kwargs.pop("request", None)
             response = func_kwargs.pop("response", None)
+            create_response_directly = False
+            if not response:
+                response = Response()
+                create_response_directly = True
             redis_cache = FastApiRedisCache()
-            if not redis_cache.connected:
-                # if the redis client is not connected to the server, no caching behavior is performed.
-                return await get_api_response_async(func, *args, **kwargs)
-            if not redis_cache.request_is_cacheable(request):
-                return await get_api_response_async(func, *args, **kwargs)
 
+            # if the redis client is not connected or request is not cacheable, no caching behavior is performed.
+            if redis_cache.not_connected or redis_cache.request_is_not_cacheable(request):
+                return await get_api_response_async(func, *args, **kwargs)
             key = redis_cache.get_cache_key(func, *args, **kwargs)
             ttl, in_cache = redis_cache.check_cache(key)
             if in_cache:
-                cached_data = redis_cache.deserialize_json(in_cache)
-                if response and redis_cache.requested_resource_not_modified(request, cached_data):
+                if redis_cache.requested_resource_not_modified(request, in_cache):
                     response.status_code = HTTPStatus.NOT_MODIFIED
                     return response
-                if response:
-                    redis_cache.set_response_headers(response, cache_hit=True, response_data=cached_data, ttl=ttl)
-                return redis_cache.deserialize_json(in_cache)
+                cached_data = redis_cache.deserialize_json(in_cache)
+                redis_cache.set_response_headers(response, cache_hit=True, response_data=cached_data, ttl=ttl)
+                if create_response_directly:
+                    return Response(content=in_cache, media_type="application/json", headers=response.headers)
+                return cached_data
             response_data = await get_api_response_async(func, *args, **kwargs)
             redis_cache.add_to_cache(key, response_data, expire_after_seconds, expire_after_milliseconds)
-            if response:
-                ttl = calculate_ttl(expire_after_seconds, expire_after_milliseconds)
-                redis_cache.set_response_headers(response, cache_hit=False, response_data=response_data, ttl=ttl)
+            ttl = calculate_ttl(expire_after_seconds, expire_after_milliseconds)
+            redis_cache.set_response_headers(response, cache_hit=False, response_data=response_data, ttl=ttl)
+            if create_response_directly:
+                return Response(
+                    content=redis_cache.serialize_json(response_data),
+                    media_type="application/json",
+                    headers=response.headers,
+                )
             return response_data
-
-        def calculate_ttl(expire_s=0, expire_ms=0):
-            if not expire_s and not expire_ms:
-                return -1
-            ttl = expire_s + math.floor(expire_ms / 1000)
-            return ttl or 1
-
-        async def get_api_response_async(func, *args, **kwargs):
-            """Helper function that allows decorator to work with both async and non-async functions."""
-            return await func(*args, **kwargs) if asyncio.iscoroutinefunction(func) else func(*args, **kwargs)
 
         return inner_wrapper
 
     return outer_wrapper
+
+
+async def get_api_response_async(func, *args, **kwargs):
+    """Helper function that allows decorator to work with both async and non-async functions."""
+    return await func(*args, **kwargs) if asyncio.iscoroutinefunction(func) else func(*args, **kwargs)
+
+
+def calculate_ttl(expire_s, expire_ms):
+    if (not expire_s and not expire_ms) or (expire_s == 0 and expire_ms == 0):
+        return -1
+    expire_s = expire_s or 0
+    expire_ms = expire_ms or 0
+    ttl = expire_s + math.floor(expire_ms / 1000)
+    return ttl or 1
